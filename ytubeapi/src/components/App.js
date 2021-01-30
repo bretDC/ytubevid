72806bfdb1af4ccc16e086fe4170cfa48e1eebf6
Bret <bretbickhamx1989@gmail.com>
2021-01-30 11:30:23 -0700

    fixed autocorrected KEY issue

--- ytubeapi/src/components/App.js
+++ ytubeapi/src/components/App.js
@@ -20,7 +20,7 @@ class App extends React.Component {
                 part: 'snippet',
                 maxResults: 5,
                 type: 'video',
-                key: KeyboardEvent,
+                key: KEY
             },
         });
 
@@ -37,7 +37,7 @@ class App extends React.Component {
     render() {
         return (
           <div className="ui container">
-              <SearchBar onFormSubmit={this.onFormSubmit} />
+              <SearchBar onFormSubmit={this.onTermSubmit} />
               <div className="ui grid">
                   <div className="ui row">
                       <div className="eleven wide column">
